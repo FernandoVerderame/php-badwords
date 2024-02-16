61998771f582a10ae1664282424603a93ad25017
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-16 17:09:48 +0100

    print censored paragraph on page

--- script/script.php
+++ script/script.php
@@ -6,3 +6,9 @@ $bad_word = $_GET['text'];
 echo $paragraph;
 
 echo strlen(trim($paragraph));
+
+$new_paragraph = str_replace($bad_word, '***', $paragraph);
+
+echo $new_paragraph;
+
+var_dump(trim($new_paragraph));
