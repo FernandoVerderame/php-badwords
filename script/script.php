52d325ed1074e0e22b2e02ad1fd52575fcdca289
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-16 17:11:04 +0100

    added comments

--- script/script.php
+++ script/script.php
@@ -1,14 +1,18 @@
 <?php
 
+// Variables
 $paragraph = $_GET['paragraph'];
 $bad_word = $_GET['text'];
 
+// Print paragraph
 echo $paragraph;
 
+// Print paragraph lenght
 echo strlen(trim($paragraph));
 
+// New paragraph
 $new_paragraph = str_replace($bad_word, '***', $paragraph);
 
+// Print new paragraph
 echo $new_paragraph;
-
 var_dump(trim($new_paragraph));
